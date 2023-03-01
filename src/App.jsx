4378aed57d8a330c5d5d5d9acba72aca91c683df
Erika Zarate Gutierrez <erikazg20@gmail.com>
2023-03-01 09:30:49 -0600

    Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  const nombre="Erika Zárate Gutiérrez";
+  return (
+    <div>
+      <h3>Lista de Hobbies de {nombre}</h3>
+      <ol>
+        <li>Jugar Videojuegos</li>
+        <li>Leer</li>
+        <li>Tocar la Guitarra</li>
+      </ol>
+    </div>
+  )
+}
+export default App
